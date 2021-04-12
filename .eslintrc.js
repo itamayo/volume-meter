a47b19c31be28925f5939738560ea6728083fd33
itamayo@vicomtech.org <itamayo@vicomtech.org>
2021-04-12 14:03:20 +0200

    [initial]:version 0.0.1

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+
+module.exports = {
+  parser: "babel-eslint",
+  extends: [
+	],
+	plugins: [
+	],
+	env: {
+		es6: true,
+		browser: true,
+		node: true,
+	},
+	parserOptions: {
+		ecmaVersion: 2017,
+		sourceType: 'module',
+	},
+	rules: {
+	},
+	settings: {
+	},
+};
